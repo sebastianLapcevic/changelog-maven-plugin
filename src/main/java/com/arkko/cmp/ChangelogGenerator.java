0bc1a0de604b4088c7cef9e41b57d433b60e829f
sebastianLapcevic <sebaslamu@gmail.com>
2015-10-19 17:11:37 -0600

    Removes the usage of the xml file, generates the changelog.md file in the /target directory

--- src/main/java/com/arkko/cmp/ChangelogGenerator.java
+++ src/main/java/com/arkko/cmp/ChangelogGenerator.java
@@ -1,10 +1,12 @@
 package com.arkko.cmp;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map.Entry;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -25,48 +27,58 @@ import org.w3c.dom.Element;
 public class ChangelogGenerator {
 
     private static final Logger log = Logger.getLogger(ChangelogGenerator.class.getName());
-    
-    DocumentBuilderFactory docFactory;
-    DocumentBuilder docBuilder;
-    Document doc;
-    Element rootElement;
+
     String markdown;
-    
+
     public void generateChangelog(HashMap<String, HashMap<String, List<HashMap<String, String>>>> versions) throws ParserConfigurationException, TransformerException, IOException {
         log.info("Start");
-        
-        this.docFactory = DocumentBuilderFactory.newInstance();
-        this.docBuilder = docFactory.newDocumentBuilder();
-        this.doc = docBuilder.newDocument();
-        
-        markdown = markdown + "# Changelog \n";
-        
-        log.info("Getting versions: "+ versions);
+        markdown = "# Changelog \n";
+
+        log.log(Level.INFO, "Getting versions: {0}", versions);
         for (Entry<String, HashMap<String, List<HashMap<String, String>>>> entry : versions.entrySet()) {
             String key = entry.getKey();
-            log.info("LLAVE: " + key);
+            log.log(Level.INFO, "LLAVE: {0}", key);
             HashMap value = entry.getValue();
             generateVersion(value, key);
         }
+        generateFile();
         
-        log.info("Creating transformer Factory");
-        TransformerFactory transformerFactory = TransformerFactory.newInstance();
-        Transformer transformer = transformerFactory.newTransformer();
-        DOMSource source = new DOMSource(doc);
-        StreamResult result = new StreamResult(new File("target/changelog.xml"));
-        
-        Markdown4jProcessor processor = new Markdown4jProcessor();
-        processor.process(markdown);
-
-        transformer.transform(source, result);
         log.info("Done");
+    }
+    
+    private void generateFile() throws IOException{
+        Markdown4jProcessor processor = new Markdown4jProcessor();
+        String mdFile = processor.process(markdown);
+        File file;
+        FileOutputStream fop = null;
+        try {
+            file = new File("target/changelog.md");
+            fop = new FileOutputStream(file);
+            
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            byte[] contentInBytes = mdFile.getBytes();
+
+            fop.write(contentInBytes);
+            fop.flush();
+            fop.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (fop != null) {
+                    fop.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     private void generateVersion(HashMap<String, List<HashMap<String, String>>> commands, String versionName) {
-        rootElement = doc.createElement(versionName);
-        doc.appendChild(rootElement);
-        
-        markdown = markdown + "### "+versionName+" \n";
+        markdown = markdown + "### " + versionName + " \n";
 
         appendCommits(commands.get("feat"), "Features");
         appendCommits(commands.get("fix"), "Fixes");
@@ -79,32 +91,13 @@ public class ChangelogGenerator {
 
     private void appendCommits(List<HashMap<String, String>> commits, String commandName) {
         if (!commits.isEmpty()) {
-            Element command = doc.createElement(commandName);
-            rootElement.appendChild(command);
             for (HashMap<String, String> commit : commits) {
-                Element commitElement = doc.createElement("commit");
-
-                markdown = markdown + "... *"+commit.get("scope").trim()+"*:";
-                markdown = markdown + commit.get("subject")+ "\n";
-                if(!commit.get("body").isEmpty()){
-                    markdown = markdown + "... ..."+commit.get("body").trim()+" \n";
-                    markdown = markdown + "... ..."+commit.get("footer").trim()+" \n";
+                markdown = markdown + "- **" + commit.get("scope").trim() + "**:";
+                markdown = markdown + commit.get("subject") + "\n";
+                if (!commit.get("body").isEmpty()) {
+                    markdown = markdown + "..-" + commit.get("body").trim() + " \n";
+                    markdown = markdown + "..-" + commit.get("footer").trim() + " \n";
                 }
-                
-                Attr attr = doc.createAttribute("subject");
-                attr.setValue(commit.get("subject"));
-                commitElement.setAttributeNode(attr);
-                attr = doc.createAttribute("body");
-                attr.setValue(commit.get("body"));
-                commitElement.setAttributeNode(attr);
-                attr = doc.createAttribute("footer");
-                attr.setValue(commit.get("footer"));
-                commitElement.setAttributeNode(attr);
-                attr = doc.createAttribute("scope");
-                attr.setValue(commit.get("scope"));
-                commitElement.setAttributeNode(attr);
-
-                command.appendChild(commitElement);
             }
         }
     }
